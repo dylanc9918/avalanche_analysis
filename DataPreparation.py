212fefbffe37655b614b5995e53950343cf9b1eb
dylanc9918 <dylanc9918@gmail.com>
2025-05-28 15:02:49 -0700

    updated weather api to deal with more historical weather data. added data prep steps to make it easier to work with for model

--- DataPreparation.py
+++ DataPreparation.py
@@ -10,7 +10,7 @@ avalanche_summary = pd.read_sql_query(
     "SELECT id, elevation, start_angle, aspect FROM avalanches_nonfatal", engine)
 
 weather_api = pd.read_sql_query(
-    "SELECT id, wind_dir, wind_speed_max, snowfall_sum, temperature_max FROM weather_api", engine)
+    "SELECT * FROM weather_api_complete", engine)
 
 
 forecast_summary = pd.read_sql_query(
@@ -20,13 +20,43 @@ forecast_problems = pd.read_sql(
     "SELECT id,problem, elevation AS problem_elevation, aspect AS problem_aspect,likelihood, size FROM forecast_problems", engine)
 
 
+incident_dates = weather_api.groupby('id')['date'].max().reset_index()
+incident_dates = incident_dates.rename(columns={'date': 'incident_date'})
+
+weather_api = pd.merge(weather_api, incident_dates, on='id', how='left')
+
+# Calculate n_days_before for each row
+weather_api['n_days_before'] = (
+    weather_api['incident_date'] - weather_api['date'])
+
+
+weather_api['n_days_before'] = weather_api['n_days_before'].map(
+    lambda x: x.days)
+
+# Create the n_label column (e.g., n_0, n_1, ...)
+weather_api['date'] = 'n_' + weather_api['n_days_before'].astype(str)
+
+weather_vars = [col for col in weather_api.columns if col not in [
+    'id', 'date', 'incident_date', 'n_days_before']]
+
+weather_pivot = weather_api.pivot_table(
+    index='id',
+    columns='date',
+    values=weather_vars
+)
+
+# Flatten MultiIndex columns: (max_temp, n_0) -> n_0_max_temp
+weather_pivot.columns = [f"{col[0]}_{col[1]}" for col in weather_pivot.columns]
+weather_pivot = weather_pivot.reset_index()
+
+
 avalanche_merge = pd.merge(pd.merge(pd.merge(pd.merge(incident_summary, avalanche_summary,
-                           on='id'), forecast_summary, on='id'), forecast_problems, on='id'), weather_api, on='id')
+                           on='id'), forecast_summary, on='id'), forecast_problems, on='id'), weather_pivot, on='id')
 
 try:
-    existing_data = pd.read_json('merge_data.json', lines=True)
+    existing_data = pd.read_json('prepped_merge_data.json', lines=True)
 except FileNotFoundError:
-    print("calculated_merge_data.json not found. Assuming no existing data.")
+    print("prepped_merge_data.json not found. Assuming no existing data.")
     existing_data = pd.DataFrame(columns=['id'])
 
 new_ids = set(avalanche_merge['id']) - set(existing_data['id'])
@@ -76,5 +106,5 @@ merged_data = pd.merge(
 
 merged_data = merged_data.drop_duplicates()
 
-output_file = 'prepped_merge_data.json'
+output_file = 'calculated_merge_data.json'
 merged_data.to_json(output_file, orient='records', lines=True)
